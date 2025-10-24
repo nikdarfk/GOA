621e42b5e5afa96f791b24e802a96ef7c2919dec
nikdarfk <nikolozdavari435@gmail.com>
2025-10-24 15:34:51 +0400

    first commit

---
+++ day 1/classwork/lesson.py
@@ -0,0 +1,18 @@
+name="Nick" 
+print(name) 
+
+
+#the second classwork
+
+blah1="rususdan"
+blah2="nick"
+blah3="ilia"
+print(blah1)
+print(blah2)
+print(blah3)
+
+#3rd classwork
+blah4="nick"
+blah5=15
+print(blah4)
+print(15)
---
+++ day 4/classwork.py
@@ -0,0 +1,50 @@
+
+
+
+age=int(input ("Enter your age: "))
+if age>=10:
+    print("რიცხვი არის 10 ის ტოლი")
+else:
+    print("რიცხვი არ არის 10 ის ტოლი")
+
+    #cw 2
+
+
+token=int(input("Enter your token amount: "))
+token=80
+
+if token>=100:
+    print("You can enter the club")
+else:
+    print("You cannot enter the club")
+
+    #cw 3   
+
+#cw 4
+
+
+a = float(input("შეიყვანეთ a: "))
+b = float(input("შეიყვანეთ b: "))
+if a > 0 and b > 0:
+    print("ორივე პირობა სწორია")
+else:
+    print("პირობა არასწორია")
+
+
+
+
+
+    #cw 5
+
+
+a = float(input("შეიყვანეთ a: "))
+b = float(input("შეიყვანეთ b: "))
+if a > 5 or b > 5:
+    print("ერთ-ერთი პირობა მაინც სწორია")
+else:
+    print("არც ერთი პირობა არ შესრულდა")
+
+
+
+
+
